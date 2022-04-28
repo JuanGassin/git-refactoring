284cb6343993cf90e5f1c40aea6f70ba7d0bebf1
LuisEladioSG <leladiosg@gmail.com>
2022-04-28 22:04:04 +0200

    Añadidos cambios importantes

--- js-ejercicios-refactoring/Ejercicio 4/ejer4.js
+++ js-ejercicios-refactoring/Ejercicio 4/ejer4.js
@@ -1,27 +1,37 @@
-function saludar() {
-    var lahoraqueesenestemomento = new Date().getHours();
-
-    if ((lahoraqueesenestemomento >= 6) && (lahoraqueesenestemomento <= 12)) {
-    alert('Buenos' + ' días');
+function saludar() {
+
+    let hora_actual = new Date().getHours();
+
+    if ((hora_actual >= 6) && (hora_actual <= 12)) {
+
+    alert('Buenos' + ' días');
+
+    }
+
+    if ((hora_actual >= 13) && (hora_actual <= 20)) {
+
+    alert('Buenas tardes');
+
+    } 
+
+    if(hora_actual > 20){
+    alert('Buenas noches');
+    }
+
+    if ((hora_actual >= 6) && (hora_actual <= 12)){
+        document.write('Es por la mañanita')
+    }
+
+    if ((hora_actual >= 13) && (hora_actual <= 20)) {
+
+        document.write('Es por la tarde');
+
+        } 
+
+        if(hora_actual > 20){
+
+            document.write('Es por la noche');
+
+            }
     }
-
-    if ((lahoraqueesenestemomento >= 13) && (lahoraqueesenestemomento <= 20)) {
-    alert('Buenas' + ' tardes');
-    } 
-
-    if(lahoraqueesenestemomento > 20){
-    alert('Buenas' + ' noches');
-    }
-
-    if ((lahoraqueesenestemomento >= 6) && (lahoraqueesenestemomento <= 12)){
-        document.write('Es por la mañanita')
-    }
-
-    if ((lahoraqueesenestemomento >= 13) && (lahoraqueesenestemomento <= 20)) {
-        document.write('Es por la tarde');
-        } 
-
-        if(lahoraqueesenestemomento > 20){
-            document.write('Es por la noche');
-            }
-    }
+}
